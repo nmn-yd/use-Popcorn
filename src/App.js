57b40c6c0e7b80d5c7f3b3e04779cc1be80c6a0e
Naman Yadav <raymannky2001@gmail.com>
2023-08-08 19:50:10 +0530

    adding effects to project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -46,13 +46,28 @@ const tempWatchedData = [
     userRating: 9,
   },
 ];
-
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "82f180af";
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  useEffect(function () {
+    async function fetchMovies() {
+      const res =
+        await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=the dark night
+      `);
+
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+
+    fetchMovies();
+  }, []);
+
   return (
     <>
       <Navbar>
